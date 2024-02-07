8cf3c879da65f91c6c9ce261e8b48205076ee257
KNULL-git <72454460+KNULL-git@users.noreply.github.com>
2024-02-07 10:09:40 +0530

    Create about.js

---
+++ about.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let aboutLink = document.querySelector('a[href="#ABOUT"]');
+    
+    aboutLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        // Your code to handle navigating to the about page goes here
+        window.location.href = "about.html"; // Replace "about.html" with the actual URL of your about page
+    });
+});
