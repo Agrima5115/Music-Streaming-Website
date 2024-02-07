810843259b57826e6aaecc98944ebc01c8983ce9
Agrima <agrimarajput095@gmail.com>
2024-02-07 22:51:04 +0530

    navigation added

---
+++ artist/artists.js
@@ -0,0 +1,9 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     let artistsLink = document.querySelector('a[href="#ARTISTS"]');
+    
+//     artistsLink.addEventListener("click", function(event) {
+//         event.preventDefault();
+//         // Your code to handle navigating to the artists page goes here
+//         window.location.href = "artists.html"; // Replace "artists.html" with the actual URL of your artists page
+//     });
+// });
---
+++ contact/contact.js
@@ -0,0 +1,16 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     let contactLink = document.querySelector('a[href="#CONTACT"]');
+    
+//     contactLink.addEventListener("click", function(event) {
+//         event.preventDefault();
+//         // Your code to handle navigating to the contact page goes here
+//         // For example, you can use window.location.href to redirect to the contact page
+//         window.location.href = "contact.html"; // Replace "contact.html" with the actual URL of your contact page
+//     });
+// });
+// document.querySelector('#contact-form').addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     e.target.elements.name.value = '';
+//     e.target.elements.email.value = '';
+//     e.target.elements.message.value = '';
+//   });
